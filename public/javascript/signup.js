7f7b153fd806c070c5c2ee4811c1b3791db50411
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-05-23 02:29:59 -0500

    signing up is updated

---
+++ public/javascript/signup.js
@@ -0,0 +1,5 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
