e158c0692a511213959595f5a4f93b193ac7d80b
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-05-23 20:47:13 -0500

    all error are fixed

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-
+//let userRoutes = require('./user-routes.js');
+// let  postRoutes = require('./post-routes.js');
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
 const commentRoutes = require('./comment-routes.js');
+// const router.use('/comments', commentRoutes)
+//const router.use('/posts', postRoutes);
+//const router.use('/posts', postRoutes);
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
