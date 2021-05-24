94aeee7d821975206ac94daa76f2140c9cf6d9ff
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-05-23 20:59:58 -0500

    update

--- utils/helper.js
+++ utils/helper.js
@@ -1,10 +1,12 @@
 function formatDate(date) {
+    // return `${new Date(date).getMonth() 0}
     return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
         date
       ).getFullYear()}`;
 }
 
-
+// formatDate
+// }
 module.exports = {
     formatDate
 }
