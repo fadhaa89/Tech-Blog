da70ae9cbd74a6333713597b9be095f42f89709a
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-05-23 20:09:22 -0500

    username to single-post page is changed and updated

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,18 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+// let apiRoutes = require('./api');
+//let  homeRoutes = require;
+//let dashBoardRoutes = require;
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
-
-
+//////////////////////---api------------/////////////////
+//router.use('/api';apiroutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
+//router.use(dashBoard, dashboardRoutes);
 router.use('/', homeRoutes);
 
-
+/////////--req,res--///////
 router.use((req, res) => {
     res.status(404).end();
 });
